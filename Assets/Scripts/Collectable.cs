96e94f7c441414bfb5519e1887583163c156359f
makaylaFar <mfarley11@student.cscc.edu>
2024-10-23 13:01:06 -0400

    ahyhhhh

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -5,82 +5,21 @@ using UnityEngine.UI;
 
 public class Collectable : MonoBehaviour
 {
-    [Tooltip("UI text to show when the player can collect the item")]
-    public GameObject collectTextUI; 
-    private float _items = 0;
-    [Tooltip("Maximum distance to interact with the collectable")]
-    public float interactionDistance = 3.0f;
-    public float items{
-        get {return _items;}
-        set {_items = value;}
-    }
-    
-    
+    private float items = 0;
+    public float collectableNum = 8;
 
-    void Start()
-    {
-        if (collectTextUI != null)
-        {
-            collectTextUI.SetActive(false); 
-        }
-
-        WinScreen.SetActive(false);
+    void Start(){
+        winScreen.SetActive(false);
     }
 
-    void Update()
-    {
-        lookingAtCollectable(); 
-    }
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("Collectable")){
+             other.gameObject.SetActive(false)
+             items += 1; 
 
-    
-
-    void PickUp()
-    {
-        items += 1; 
-        if (gameObject.CompareTag("Collectable")) 
-       {
-           gameObject.SetActive(false);
-       } 
-
-        if (collectTextUI != null)
-        {
-            collectTextUI.SetActive(false); 
-        }
-    }
-
-    void lookingAtCollectable()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, interactionDistance))
-        {
-            if (hit.transform == transform)
-            {
-                if (collectTextUI != null)
-                {
-                    collectTextUI.SetActive(true); // Show the UI text when in range and looking at the object
-                }
-
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    PickUp(); 
-                }
-            }
-            else
-            {
-                if (collectTextUI != null)
-                {
-                    collectTextUI.SetActive(false); // Hide the UI text if not looking at the object
-                }
-            }
-        }
-        else
-        {
-            if (collectTextUI != null)
-            {
-                collectTextUI.SetActive(false); // Hide the UI text if nothing is being hit
-            }
+             if (items == collectableNum){
+                winScreen.SetActive(true);
+             }
         }
     }
 }
